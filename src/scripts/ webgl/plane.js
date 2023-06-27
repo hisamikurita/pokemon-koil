03fdc0039df53e4e3202a6ea6082f1a1210134bc
栗田久海 <kurita_hisami@liginc.co.jp>
2023-06-28 01:26:30 +0900

    update

--- src/scripts/ webgl/plane.js
+++ src/scripts/ webgl/plane.js
@@ -23,7 +23,7 @@ export class Plane extends WebglBase {
 
   _loadModel() {
     const loader = new GLTFLoader();
-    const url = "/koil.gltf";
+    const url = "./koil.gltf";
 
     loader.load(
       url,
