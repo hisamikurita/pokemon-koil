16e6ae36f81b88cfb73f38f1499728e2453d35f2
栗田久海 <kurita_hisami@liginc.co.jp>
2023-07-03 01:22:35 +0900

    update

--- src/scripts/ webgl/plane.js
+++ src/scripts/ webgl/plane.js
@@ -1,11 +1,14 @@
+import { gsap } from "gsap";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { WebglBase } from "./webgl-base";
-import { isSp } from "../variables";
+import { DURASION, isSp } from "../variables";
 
 export class Plane extends WebglBase {
   constructor(canvas) {
     super(canvas);
 
+    this.load = document.querySelector('[data-el="load"]');
+    this.loadBar = document.querySelector('[data-el="load-bar"]');
     this.rotateBase = 0.0012;
     this.scaleBase = isSp ? 20 : 40;
     this.buffer = 500;
@@ -32,10 +35,27 @@ export class Plane extends WebglBase {
         // オブジェクトの読み込みが完了したタイミングで実行
         this._generateModels(gltf);
         this._setupModels();
+        setTimeout(() => {
+          this._opModels();
+        }, 600);
       },
       (xhr) => {
         // オブジェクトを読み込んでいるタイミングで実行
-        console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
+        const percent = (xhr.loaded / xhr.total) * 100;
+
+        gsap.to(this.loadBar, {
+          duration: DURASION.FULL,
+          ease: "power2.out",
+          clipPath: `polygon(0 0, ${percent}% 0, ${percent}% 100%, 0 100%)`,
+
+          onComplete: () => {
+            gsap.to(this.load, {
+              duration: DURASION.FULL,
+              ease: "power2.out",
+              opacity: 0,
+            });
+          },
+        });
       },
       (error) => {
         // オブジェクトの読み込みが失敗したタイミングで実行
@@ -62,22 +82,42 @@ export class Plane extends WebglBase {
         z: Math.random() * Math.PI * 2,
       };
 
-      const scaleRange = Math.random() + 1.0;
+      model.position.x = (i / this.models.length) * window.innerWidth - window.innerWidth / 2;
+      model.position.y = Math.random() * (this.positionRange.y + this.buffer * 2) - (this.positionRange.y + this.buffer * 2) / 2;
+      model.position.z = Math.random() * this.positionRange.z - this.positionRange.z / 2;
 
-      this.models[i].scale.set(this.scaleBase * scaleRange, this.scaleBase * scaleRange, this.scaleBase * scaleRange);
-
-      this.models[i].position.x = (i / this.models.length) * window.innerWidth - window.innerWidth / 2;
-      this.models[i].position.y = Math.random() * (this.positionRange.y + this.buffer * 2) - (this.positionRange.y + this.buffer * 2) / 2;
-      this.models[i].position.z = Math.random() * this.positionRange.z - this.positionRange.z / 2;
-
-      this.models[i].rotation.x = rotate.x;
-      this.models[i].rotation.y = rotate.y;
-      this.models[i].rotation.z = rotate.z;
+      model.rotation.x = rotate.x;
+      model.rotation.y = rotate.y;
+      model.rotation.z = rotate.z;
 
       model.userData = {
         speed: isSp ? Math.random() + 0.5 : Math.random() + 1.0,
         rotate,
+        scaleRange: Math.random() + 1.0,
+        scale: 0,
       };
+
+      model.scale.set(
+        this.scaleBase * model.userData.scaleRange * model.userData.scale,
+        this.scaleBase * model.userData.scaleRange * model.userData.scale,
+        this.scaleBase * model.userData.scaleRange * model.userData.scale
+      );
+    }
+  }
+
+  _opModels() {
+    for (let i = 0; i < this.models.length; i++) {
+      const model = this.models[i];
+
+      gsap.to(model.userData, {
+        duration: DURASION.FULL,
+        delay: i * 0.1,
+        ease: "power2.out",
+        scale: 1,
+        onUpdate: () => {
+          model.scale.set(this.scaleBase * model.userData.scaleRange * model.userData.scale, this.scaleBase * model.userData.scaleRange, this.scaleBase * model.userData.scaleRange);
+        },
+      });
     }
   }
 
@@ -93,6 +133,7 @@ export class Plane extends WebglBase {
     // mesh
     for (let i = 0; i < this.models.length; i++) {
       const model = this.models[i];
+      model.position.x = (i / this.models.length) * window.innerWidth - window.innerWidth / 2;
       model.position.y += model.userData.speed;
       model.rotation.x += model.userData.rotate.x * this.rotateBase;
       model.rotation.y += model.userData.rotate.y * this.rotateBase;
